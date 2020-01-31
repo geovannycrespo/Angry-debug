749f4cb304113e2e53f05ce3b04497b5bddd28cc
Geovanny Crespo <geovannycrespo@yahoo.comgit config --global user.email geovannycrespo@yahoo.comgit user.emailgit remote add origin https://github.com/geovannycrespo/codingtemple-december-gittest.gitgit push -u origin mastergit push -u origin master>
2020-01-31 10:56:51 -0600

    Angry debug

---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyDWvFNC5_KcMCUFUCbHtgWz5aYxdTzTOng",
+    authDomain: "angry-debugger.firebaseapp.com",
+    databaseURL: "https://angry-debugger.firebaseio.com",
+    projectId: "angry-debugger",
+    storageBucket: "angry-debugger.appspot.com",
+    messagingSenderId: "52617087896",
+    appId: "1:52617087896:web:28b9f5b9524e058fd7b9ed",
+    measurementId: "G-9NZ6REWDEK"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
